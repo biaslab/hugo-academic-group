fa793ebe1119d6d61da8290812571bec7b914a3a
Ivan Bocharov <bocharovia@gmail.com>
2017-08-03 18:48:39 +0200

    Fixes in the smooth scrolling implementation.

--- static/js/hugo-academic-group.js
+++ static/js/hugo-academic-group.js
@@ -1,7 +1,18 @@
 // Smooth srolling
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    if (this instanceof SVGAElement){
+      var target = $(this.href.baseVal);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        target_top = target.offset().top
+        $('html, body').animate({
+          scrollTop: target_top
+        }, 1000);
+        return false;
+      }
+    }
+    else if ((location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
